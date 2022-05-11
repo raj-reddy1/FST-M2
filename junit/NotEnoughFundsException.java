5048e3c299851a60a2df471531139158c9a95896
raj-reddy1 <93119641+raj-reddy1@users.noreply.github.com>
2022-05-11 11:09:48 +0530

    Add files via upload

---
+++ junit/NotEnoughFundsException.java
@@ -0,0 +1,14 @@
+package activityPrograms;
+
+public class NotEnoughFundsException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NotEnoughFundsException(Integer amount, Integer balance) {
+        super("Attempted to withdraw " + amount + " with a balance of " + balance);
+    }
+
+}
